8089cd07011e2f3dc302217ba45aa86ab6bf87d9
Leo P <junk@slact.net>
2019-05-03 01:45:41 -0400

    aww yeah genpatch regen

--- dev/genpatch.sh
+++ dev/genpatch.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/zsh
 version=$1
 
-echo $version
-echo `pwd`
-
 ALL_OFF="\e[1;0m"
 BOLD="\e[1;1m"
 BLUE="${BOLD}\e[1;34m"
 YELLOW="${BOLD}\e[1;33m"
+RED="${BOLD}\e[1;31m"
 GREEN="${BOLD}\e[1;32m"
 
 prompt() {
@@ -23,6 +21,20 @@ confirm() {
   esac
 }
 
+if [[ $1 == "regen" ]]; then
+  pushd ..
+    current=( `find -maxdepth 1 -type f -name "nginx-*.patch" | sort -r` )
+  popd
+  for f in $current; do
+    ver=$(print $f | sed -r 's/[^-]*-([^-]+).*/\1/')
+    echo "${YELLOW}regenerate patch for nginx-${ver}${ALL_OFF}"
+    $0 $ver yes quiet
+    if ! [ $? -eq 0 ]; then; 
+      exit 1
+    fi
+  done
+  exit
+fi
 
 base=nginx-$version
 patched=nginx-$version-proxy_protocol_vars
@@ -39,24 +51,31 @@ fi
 tar xzf $base.tar.gz
 cp $base $patched -Rf
 
-pushd ../../src
+pushd ../../src >/dev/null
 changed=( `find -type f -not -name "*.orig"` )
-popd
+popd >/dev/null
 
 for f in $changed; do
   #echo  $f
   diff  -ur ../../src/$f.orig ../../src/$f | patch $patched/src/$f
-  #ln -sf `realpath "../../src/$f"` `realpath "$patched/src/$f"`
+  if ! [ $? -eq 0 ]; then; 
+    failed=1
+  fi
 done
-
-rm $patched/src/nginx-source
-rm $patched/src/nginx
-if [[ $2 == "meld" ]]; then
-  meld -a  $patched $base
-else
-  diff -ur -x '*~' -x '*.swp' $base/src $patched/src | colordiff
+if [[ -n $failed ]]; then
+  echo "${RED}Patch failed, source is too different.${ALL_OFF}"
+  exit 1
 fi
-if confirm "Patch for $base looks ok?"; then
+rm $patched/src/nginx-source 2>/dev/null
+rm $patched/src/nginx 2>/dev/null
+if [[ $3 != "quiet" ]]; then
+  if [[ $2 == "meld" ]]; then
+    meld -a  $patched $base
+  else
+    diff -ur -x '*~' -x '*.swp' $base/src $patched/src | colordiff
+  fi
+fi
+if [[ $2 == "yes" ]] || confirm "Patch for $base looks ok?"; then
   diff -ur -x '*~' -x '*.swp' $base/src $patched/src > ../../$patched.patch
   echo "${GREEN}saved to $patched.patch${ALL_OFF}"
 else
